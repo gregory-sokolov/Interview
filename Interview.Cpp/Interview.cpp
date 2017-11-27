48f1ef4a36046bc1bf18d0083d6fe4dc1979d42a
Grigory Sokolov <grigory.sokolov.github@gmail.com>
2017-11-27 07:30:47 +0300

    Practice in C++: std::transform, std::lower_bound, std::upper_bound

--- Interview.Cpp/Interview.cpp
+++ Interview.Cpp/Interview.cpp
@@ -670,6 +670,55 @@ int _tmain(int argc, _TCHAR* argv[])
     }
     cout << endl;
 
+
+	cout << "== C++ Various ==" << endl;
+	{
+		// Projection: std::transform one vector to another
+		vector<pair<int, int>> vpii = { make_pair(1, 2), make_pair(3, 1), make_pair(5, 4), make_pair(7, 8) };
+		vector<int> vt;
+		transform(vpii.cbegin(), vpii.cend(), back_inserter(vt), [](const pair<int, int>& p) { return p.first; });
+		cout << "Source: ";
+		for (auto it = vpii.cbegin(); it != vpii.cend(); ++it)
+		{
+			cout << "(" + to_string(it->first) + "," + to_string(it->second) + ")" << (it != vpii.cend() - 1 ? ", " : "");
+		}
+		cout << endl;
+		cout << "Result: ";
+		for (auto it = vt.cbegin(); it != vt.cend(); ++it)
+		{
+			cout << *it << (it != vt.cend() - 1 ? ", " : "");
+		}
+		cout << endl << endl;
+
+		// Binary search
+		vector<int> vi01 = { 1, 1, 2, 3, 5, 7, 7, 9, 10, 12, 15, 17 };
+		auto bs1 = lower_bound(vi01.cbegin(), vi01.cend(), 7);
+		auto bs2 = lower_bound(vi01.cbegin(), vi01.cend(), 12);
+		auto bs3 = upper_bound(vi01.cbegin(), vi01.cend(), 7);
+		auto bs4 = upper_bound(vi01.cbegin(), vi01.cend(), 12);
+		auto bs5 = lower_bound(vi01.cbegin(), vi01.cend(), 4);
+		auto bs6 = lower_bound(vi01.cbegin(), vi01.cend(), 21);
+		auto bs7 = upper_bound(vi01.cbegin(), vi01.cend(), 4);
+		auto bs8 = upper_bound(vi01.cbegin(), vi01.cend(), 21);
+
+		cout << "Source (" + to_string(vi01.size()) + "): ";
+		for (auto it = vi01.cbegin(); it != vi01.cend(); ++it)
+		{
+			cout << *it << (it != vi01.cend() - 1 ? ", " : "");
+		}
+		cout << endl;
+		cout << "Value: 7, lower_bound: [" << bs1 - vi01.cbegin() << "]" << endl;
+		cout << "Value: 12, lower_bound: [" << bs2 - vi01.cbegin() << "]" << endl;
+		cout << "Value: 7, upper_bound: [" << bs3 - vi01.cbegin() << "]" << endl;
+		cout << "Value: 12, upper_bound: [" << bs4 - vi01.cbegin() << "]" << endl;
+		cout << "Value: 4, lower_bound: [" << bs5 - vi01.cbegin() << "]" << endl;
+		cout << "Value: 21, lower_bound: [" << bs6 - vi01.cbegin() << "]" << endl;
+		cout << "Value: 4, upper_bound: [" << bs7 - vi01.cbegin() << "]" << endl;
+		cout << "Value: 21, upper_bound: [" << bs8 - vi01.cbegin() << "]" << endl;
+		cout << endl;
+	}
+	cout << endl;
+
     cout << "Program has completed" << endl;
     system("PAUSE");
     return 0;
