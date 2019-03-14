ad7b240867c0dfb3b26d95822522bc8c749b24bc
Grigory Sokolov <grigory.sokolov.github@gmail.com>
2019-03-14 03:20:07 +0300

    Massive output path correction, adding project with  Linear problems

---
+++ Linear/Linear.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <utility>
+#include <vector>
+
+///
+/// Linear operations: array and matrix problems
+///
+class Linear
+{
+public:
+	/// EPI 6.14. Sudoku checker
+	/// Checks that input matrix is a valid Sudoku assignment.
+	/// Time: O(n), space: O(n)
+	static std::pair<unsigned, unsigned> SudokuChecker(const std::vector<std::vector<unsigned>>& board)
+	{
+		std::pair<unsigned, unsigned> result(0, 0);
+
+
+
+		return result;
+	}
+};
