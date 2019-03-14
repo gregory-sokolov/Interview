6447a000bcc1e609c3acc6c0f46848ea273804e5
Grigory Sokolov <grigory.sokolov.github@gmail.com>
2019-03-14 20:29:01 +0300

    EPI 6.14, Sudoku checker, part 2

--- Linear/Linear.h
+++ Linear/Linear.h
@@ -12,70 +12,55 @@ class Linear
 public:
 	/// EPI 6.14. Sudoku checker
 	/// Checks that input board matrix is a valid Sudoku assignment.
+	/// Tedious duplicate checking problem in a matrix line by line.
+	/// Firstly, check duplicates in all rows, then in all columns, and finally in all 3x3 sections.
+	/// To generalize checking procedure, for column check the matrix is transposed, 
+	/// for section check all sections are unfolded into lines with tricky index formula.
 	/// Time: O(n), space: O(n)
-	static std::pair<unsigned, unsigned> SudokuChecker(const std::vector<std::vector<unsigned>>& board, unsigned section)
+	static std::pair<unsigned, unsigned> SudokuChecker(const std::vector<std::vector<unsigned>>& board)
 	{
-		std::pair<unsigned, unsigned> result(0, 0);
-
-		bool found = false;
-		std::vector<unsigned> check(10, 0);
-		for (unsigned i = 0; i < board.size(); ++i)
+		auto result = CheckSudokuLines(board);
+		if (result.first == 0 && result.second == 0)
 		{
-			std::fill(check.begin(), check.end(), 0);
-			for (unsigned j = 0; j < board[0].size(); ++j)
+			std::vector<std::vector<unsigned>> modified(board.size(), std::vector<unsigned>(board[0].size()));
+			for (unsigned i = 0; i < board.size(); ++i)
 			{
-				if (board[i][j] > 0 && ++check[board[i][j]] > 1)
+				for (unsigned j = 0; j < board[0].size(); ++j)
 				{
-					result = std::make_pair(i + 1, j + 1);
-					found = true;
-					break;
+					modified[i][j] = board[j][i];
 				}
 			}
-			if (found)
-			{
-				break;
-			}
-		}
+			result = CheckSudokuLines(modified);
 
-		if (!found)
-		{
-			// @TODO: replace with transposing
-			for (unsigned i = 0; i < board[0].size(); ++i)
+			if (result.first == 0 && result.second == 0)
 			{
-				std::fill(check.begin(), check.end(), 0);
-				for (unsigned j = 0; j < board.size(); ++j)
+				for (unsigned i = 0; i < board.size(); ++i)
 				{
-					if (board[j][i] > 0 && ++check[board[j][i]] > 1)
+					for (unsigned j = 0; j < board[0].size(); ++j)
 					{
-						result = std::make_pair(j + 1, i + 1);
-						found = true;
-						break;
+						modified[3*(i/3) + j/3][3*(i%3) + j%3] = board[i][j];
+						
 					}
 				}
-				if (found)
-				{
-					break;
-				}
+				result = CheckSudokuLines(modified);
 			}
 		}
 
-		std::vector<std::vector<unsigned>> unfolded(board.size(), std::vector<unsigned>(board[0].size()));
-		unsigned size = board.size();
-		for (unsigned i = 0; i < board.size(); ++i)
+		return result;
+	}
+	/// Helper method to check Sudoku validity by lines
+	static std::pair<int, int> CheckSudokuLines(const std::vector<std::vector<unsigned>>& mx)
+	{
+		std::pair<int, int> result(0, 0);
+
+		std::vector<unsigned> check(10, 0);
+		bool found = false;
+		for (unsigned i = 0; i < mx.size(); ++i)
 		{
 			std::fill(check.begin(), check.end(), 0);
-			for (unsigned j = 0; j < board[0].size(); ++j)
+			for (unsigned j = 0; j < mx[0].size(); ++j)
 			{
-				//unsigned k = (j/section)*section + j; 012, 345
-				//unfolded[(i/section) (j/section)][(j/section) ] = board[i][j];
-			}
-		}
-		for (unsigned i = 0; i < unfolded.size(); ++i)
-		{
-			std::fill(check.begin(), check.end(), 0);
-			for (unsigned j = 0; j < unfolded[0].size(); ++j)
-			{
-				if (unfolded[i][j] > 0 && ++check[unfolded[i][j]] > 1)
+				if (mx[i][j] > 0 && ++check[mx[i][j]] > 1)
 				{
 					result = std::make_pair(i + 1, j + 1);
 					found = true;
@@ -87,7 +72,6 @@ public:
 				break;
 			}
 		}
-
 
 		return result;
 	}
