8fc0fe9da21a171b2e0030a2b525e1d5f8df72ea
Grigory Sokolov <grigory.sokolov.github@gmail.com>
2019-03-14 22:12:03 +0300

    EPI 6.14, Sudoku checker, part 3

--- Linear/Linear.h
+++ Linear/Linear.h
@@ -3,6 +3,7 @@
 #include <utility>
 #include <vector>
 #include <algorithm>
+#include <cmath>
 
 ///
 /// Linear operations: array and matrix problems
@@ -10,15 +11,23 @@
 class Linear
 {
 public:
-	/// EPI 6.14. Sudoku checker
+	/// EPI 6.14.1. Sudoku checker
 	/// Checks that input board matrix is a valid Sudoku assignment.
 	/// Tedious duplicate checking problem in a matrix line by line.
 	/// Firstly, check duplicates in all rows, then in all columns, and finally in all 3x3 sections.
 	/// To generalize checking procedure, for column check the matrix is transposed, 
 	/// for section check all sections are unfolded into lines with tricky index formula.
 	/// Time: O(n), space: O(n)
-	static std::pair<unsigned, unsigned> SudokuChecker(const std::vector<std::vector<unsigned>>& board)
+	static std::pair<int, int> SudokuChecker1(const std::vector<std::vector<unsigned>>& board)
 	{
+		unsigned section = (unsigned)std::sqrt(board.size());
+		if (board.empty() ||
+			board.size() != board[0].size() ||
+			section*section != board.size())
+		{
+			return std::make_pair(-1, -1);
+		}
+
 		auto result = CheckSudokuLines(board);
 		if (result.first == 0 && result.second == 0)
 		{
@@ -31,6 +40,7 @@ public:
 				}
 			}
 			result = CheckSudokuLines(modified);
+			result = std::make_pair(result.second, result.first);
 
 			if (result.first == 0 && result.second == 0)
 			{
@@ -38,7 +48,7 @@ public:
 				{
 					for (unsigned j = 0; j < board[0].size(); ++j)
 					{
-						modified[3*(i/3) + j/3][3*(i%3) + j%3] = board[i][j];
+						modified[section*(i/section) + j/section][section*(i%section) + j%section] = board[i][j];
 						
 					}
 				}
@@ -70,6 +80,77 @@ public:
 			if (found)
 			{
 				break;
+			}
+		}
+
+		return result;
+	}
+
+	/// EPI 6.14.2 Sudoku checker
+	/// Does the same check with less space (bit vector, no matrix copying) but with code duplication.
+	/// Uses another method of section check.
+	/// Time: O(n), space: O(1)
+	static std::pair<int, int> SudokuChecker2(const std::vector<std::vector<unsigned>>& board)
+	{
+		unsigned section = (unsigned)std::sqrt(board.size());
+		if (board.empty() ||
+			board.size() != board[0].size() ||
+			section * section != board.size())
+		{
+			return std::make_pair(-1, -1);
+		}
+
+		std::pair<int, int> result(0, 0);
+		std::vector<bool> check(10, false);
+		for (unsigned i = 0; i < board.size(); ++i)
+		{
+			std::fill(check.begin(), check.end(), false);
+			for (unsigned j = 0; j < board[0].size(); ++j)
+			{
+				if (board[i][j] > 0 && check[board[i][j]])
+				{
+					return std::make_pair(i + 1, j + 1);
+				}
+				else
+				{
+					check[board[i][j]] = true;
+				}
+			}
+		}
+		for (unsigned i = 0; i < board[0].size(); ++i)
+		{
+			std::fill(check.begin(), check.end(), false);
+			for (unsigned j = 0; j < board.size(); ++j)
+			{
+				if (board[j][i] > 0 && check[board[j][i]])
+				{
+					return std::make_pair(j + 1, i + 1);
+				}
+				else
+				{
+					check[board[j][i]] = true;
+				}
+			}
+		}
+		for (unsigned I = 0; I < section; ++I)
+		{
+			for (unsigned J = 0; J < section; ++J)
+			{
+				std::fill(check.begin(), check.end(), false);
+				for (unsigned i = 0; i < section; ++i)
+				{
+					for (unsigned j = 0; j < section; ++j)
+					{
+						if (board[section*I + i][section*J + j] > 0 && check[board[section*I + i][section*J + j]])
+						{
+							return std::make_pair(section*I + i + 1, section*J + j + 1);
+						}
+						else
+						{
+							check[board[section*I + i][section*J + j]] = true;
+						}
+					}
+				}
 			}
 		}
 
