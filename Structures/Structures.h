4acfbde38e2f8493b227d4507e9baa0b8c1ddfcc
Grigory Sokolov <grigory.sokolov.github@gmail.com>
2020-02-04 06:37:29 +0300

    Corrections in Leetcode 146, 'LRU Cache'

--- Structures/Structures.h
+++ Structures/Structures.h
@@ -4,43 +4,57 @@
 #include <unordered_map>
 
 ///
-/// LRU Cache
+/// Leetcode 146. LRU Cache
 ///
 template<typename K, typename T>
 class LruCache
 {
 	int size;
 	std::list<std::pair<K, T>> ql;
-	std::unordered_map<K, typename std::list<std::pair<K, T>>::const_iterator> ht;
+	std::unordered_map<K, typename std::list<std::pair<K, T>>::iterator> ht;
 
 public:
 	LruCache(int sz) : size(sz)
 	{
 	}
 
-	void Insert(K id, const T& item)
+	/// Returns an item by key, null - if not found.
+	/// Moves requested item to the top of the cache.
+	const T* Get(K key)
 	{
-		if (ql.size() == size)
-		{
-			ht.erase(ql.back().first);
-			ql.pop_back();
-		}
-
-		ql.push_front(std::make_pair(id, item));
-		ht[id] = ql.cbegin();
-	}
-
-	const T* Get(K id)
-	{
-		auto it = ht.find(id);
+		auto it = ht.find(key);
 		if (it != ht.cend())
 		{
 			ql.splice(ql.cbegin(), ql, it->second);
 		}
 
-		return it != ht.cend() ? &((it->second)->second) : NULL;
+		return it != ht.cend() ? &((it->second)->second) : nullptr;
 	}
 
+	/// Puts an item to the cache by key: either updates the existing or inserts a new one.
+	/// Each time moves the item to the top of the cache.
+	void Put(K key, const T& item)
+	{
+		auto it = ht.find(key);
+		if (it != ht.cend())
+		{
+			*(it->second) = std::make_pair(key, item);
+			ql.splice(ql.begin(), ql, it->second);
+			ht[key] = it->second;
+		}
+		else
+		{
+			if (ql.size() == size)
+			{
+				ht.erase(ql.back().first);
+				ql.pop_back();
+			}
+			ql.push_front(std::make_pair(key, item));
+			ht[key] = ql.begin();
+		}
+	}
+
+	/// Returns all cache items
 	const std::list<std::pair<K, T>>& GetAll() const
 	{
 		return ql;
